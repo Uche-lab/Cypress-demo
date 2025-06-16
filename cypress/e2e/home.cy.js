bf260b3eda61536e7eb2cb38da9d6f53f6374e69
uchechukwu E. kalu <uchechukwue.kalu@uchechukwus-air.home>
2025-06-15 22:09:53 -0400

    Break test to simulate failure

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,8 @@
 describe('Simple test', () => {
   it('visits a page and checks content', () => {
     cy.visit('https://example.cypress.io');
-    cy.contains('Nonexistent Text').should('exist');
     cy.contains('type').should('be.visible');
     cy.contains('docs.cypress.io').should('be.visible').click();
-    
+    cy.contains('Nonexistent Text').should('exist');
   })
 });
